88863f20521ed2dc6230a7d6928c3c8348c24e31
Andreas <andreas.just.petersen@gmail.com>
2024-02-28 11:55:38 +0100

    Clap

--- csp_project_1/src/main.rs
+++ csp_project_1/src/main.rs
@@ -1,32 +1,61 @@
-use std::{env, fs::File, io::{self, Write}};
+use clap::{Args, Parser, Subcommand, ValueEnum};
 use rand::Rng;
+use std::{
+    env,
+    fs::File,
+    io::{self, Write},
+};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-        println!("Pls give args");
-    }
-    let num_threads = args[1].parse::<i32>().expect("parsing num threads failed");
-    let num_hash_bits = args[2].parse::<i32>().expect("parsing hash bits failed");
-    let partitioning_method = args[3].parse::<i32>().expect("parsing partition method failed");
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    match partitioning_method {
-        1 => independent_output(num_threads, num_hash_bits),
-        2 => count_then_move(num_threads, num_hash_bits),
-        _ => panic!("Invalid partitioning method! Pls give 1 or 2")
-    }
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Gen { size: usize, file: String },
 
+    #[command(arg_required_else_help = true)]
+    Run {
+        num_threads: i32,
+        num_hash_bits: i32,
+        partitioning_method: i32,
+    },
+}
 
-    println!("#threads {} #bits {} #part method{}", num_threads, num_hash_bits, partitioning_method);
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Gen { size, file } => gen_data(size, file.as_str()),
+        Commands::Run {
+            num_threads,
+            num_hash_bits,
+            partitioning_method,
+        } => {
+                println!(
+                    "#threads {} #bits {} #part method{}",
+                    num_threads, num_hash_bits, partitioning_method
+                );
+            match partitioning_method {
+                1 => independent_output(num_threads, num_hash_bits),
+                2 => count_then_move(num_threads, num_hash_bits),
+                _ => panic!("Invalid partitioning method! Pls give 1 or 2"),
+            };
+            Ok(())
+        }
+    }?;
 
 
     //DATA GENERATION
     //we need to do it ourselves
     //and do it in a way where reading from disk
-    //is NOT included in our measurements e.g. 
-    //generate just before experiement or make sure 
+    //is NOT included in our measurements e.g.
+    //generate just before experiement or make sure
     //that it is read into memory (not streamed) just before
-
+    Ok(())
 }
 
 fn hash(part_key: i64, hash_bits: i64) -> i64 {
@@ -37,22 +66,19 @@ fn hash(part_key: i64, hash_bits: i64) -> i64 {
 fn independent_output(num_threads: i32, num_hash_bits: i32) {
     //coordination of input tuples to each thread is necessary
     //suggestion: divide input in num_threads blocks and assign
-    
+
     //create output buffers for each thread, for each partition
     //t*(2^b) output buffers
     //where b is the number of hash bits
     //do hash key % hash bits
     //where hash bits is 1-18
-    let N: i32 = 90000000; //placeholder -> replace with 
-    let buffer_size: i32 = N / (num_threads*(2 << num_hash_bits));
-    let num_buffers: i32 = num_threads*(2 << num_hash_bits);
+    let N: i32 = 90000000; //placeholder -> replace with
+    let buffer_size: i32 = N / (num_threads * (2 << num_hash_bits));
+    let num_buffers: i32 = num_threads * (2 << num_hash_bits);
     let buffers: Vec<Vec<u64>> = vec![vec![0; buffer_size as usize]; num_buffers as usize];
-
 }
 
-fn count_then_move(num_threads: i32, num_hash_bits: i32) {
-
-}
+fn count_then_move(num_threads: i32, num_hash_bits: i32) {}
 
 fn gen_data(size: usize, file: &str) -> io::Result<()> {
     println!("Writing {} tuples to {}...", size, file);
