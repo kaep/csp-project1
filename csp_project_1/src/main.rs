14fccf465e9eed62b6fea7ebcc019092c2c71307
kaep <sebastianholmgaard@live.dk>
2024-02-28 10:13:48 +0100

    take amount of threads as cmd arg

--- csp_project_1/src/main.rs
+++ csp_project_1/src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let num_threads = args[1].parse::<i32>().expect("cmd args parsing");
+    println!("Amount of threads {}", num_threads);
 }
