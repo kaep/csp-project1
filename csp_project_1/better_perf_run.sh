f0da3a4b3579fc241a59d5e65369890e14d0bc68
kaep <sebastianholmgaard@live.dk>
2024-03-11 15:07:07 +0100

    more metrics

--- csp_project_1/better_perf_run.sh
+++ csp_project_1/better_perf_run.sh
@@ -4,7 +4,7 @@ do
 	do
 		for num_bits in {1..18};
 		do
-			perf stat -o results/$part_method-$num_threads-$num_bits.txt -e cycles,instructions,iTLB-load-misses,dTLB-load-misses cargo +nightly run run $num_threads $num_bits $part_method
+			perf stat -o results/$part_method-$num_threads-$num_bits.txt -e cycles,instructions,L1-icache-load-misses,L1-dcache-load-misses,LLC-load-misses,cache-misses,uops_retired.stall_cycles,branch-misses,iTLB-load-misses,dTLB-load-misses cargo +nightly run run $num_threads $num_bits $part_method
 		done
 	done
 done
